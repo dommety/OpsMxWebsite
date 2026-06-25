c0adb74923600817d9616aaf1fbf683bb303a632
modernmlwr <modernmalware@gmail.com>
2026-06-26 00:40:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import prerender from '@prerenderer/rollup-plugin'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    prerender({
-      routes: ['/'],
-      renderer: '@prerenderer/renderer-puppeteer',
-      rendererOptions: {
-        renderAfterTime: 5000,
-        launchOptions: { args: ['--no-sandbox', '--disable-setuid-sandbox'] },
-      },
-    }),
-  ],
+  plugins: [react()],
   // Served under /opsmx/ on the VM behind nginx (also works at root in dev)
   base: process.env.BASE_PATH || '/',
 })
